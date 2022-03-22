35eefeb8ca6fa5217cdcc44bcb9c3aba7bf52cf0
victor nwachukwu <khameleonstorm@gmail.com>
2022-03-22 15:35:52 +0100

    meta commit

--- trustwallet/settings.py
+++ trustwallet/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+%0io**rf+@bh6)9)rs%1iax$zgisk=ior%0kk*tbq4q)#nq!h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
